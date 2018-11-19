d74200a174e427b08a3a1c0fd02455be00abca75
Jonas <jonas.jore@gmail.com>
2018-11-19 23:51:23 +0100

    first commit :coffee: :joy:

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="body-main">
+    <AppNavBar/>
+    <AppWelcome msg="Velkommen til FOdataservice's hjemmeside!"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import AppWelcome from './components/AppWelcome.vue';
+import AppNavBar from './components/AppNavBar.vue';
+@Component({
+  components: {
+    AppWelcome,
+    AppNavBar,
+  },
+})
+export default class App extends Vue {
+}
+
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/AppWelcome.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mainContent">
+    <div class="welcomeBox" id="particles-js">
+      <h2 class="welcomeMessage">{{ msg }}</h2>
+      <h2>hello</h2>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import * as particles from '../assets/particles.js';
+
+@Component
+export default class AppWelcome extends Vue {
+  @Prop() private msg!: string;
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  @import '../styles/_welcomeBox.scss';
+</style>
