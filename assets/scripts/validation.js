b253f27ca996f92774b5afcee2c60314eb396fea
JonasJore <jonas.jore@gmail.com>
2019-02-25 23:37:18 +0100

    endret på foreløpig feedback av feil i kontaktskjema til frontend, kun console.logging for øyeblikket :coffee:

--- assets/scripts/validation.js
+++ assets/scripts/validation.js
@@ -1,10 +1,10 @@
 // TODO: validation.js er ikke lenger ett forklarende nok filnavn :I
 
 const validering = {
-  'INVALID_MAIL':'INVALID_MAIL', 
-  'INVALID_SUBJECT':'INVALID_SUBJECT',
-  'INVALID_TYPE_OF_SERVICE':'INVALID_TYPE_OF_SERVICE',
-  'INVALID_TEXT':'INVALID_TEXT'
+  INVALID_MAIL: 'INVALID_MAIL', 
+  INVALID_SUBJECT: 'INVALID_SUBJECT',
+  INVALID_TYPE_OF_SERVICE: 'INVALID_TYPE_OF_SERVICE',
+  INVALID_TEXT: 'INVALID_TEXT'
 }
 Object.freeze(validering)
 
@@ -14,12 +14,15 @@ let validation = (email, subject, type, text) => {
   const validationRules = []
   if(email === '' || !validEmail) {
     validationRules.push(validering.INVALID_MAIL)
+    console.log(validering.INVALID_MAIL)
   } if(subject.length == 0) {
     validationRules.push(validering.INVALID_SUBJECT)
+    console.log(validering.INVALID_SUBJECT)
   } if(type === 'default') {
     validationRules.push(validering.INVALID_TYPE_OF_SERVICE)
   } if(text.length === 0 || text.length > 150) {
     validationRules.push(validering.INVALID_TEXT)
+    console.log(validering.INVALID_TEXT)
   } 
   
   if(noErrors(validationRules)) {
