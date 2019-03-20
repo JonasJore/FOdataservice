3edcaf4eba3247ecd7442e2297abe8d2b0a14f5a
JonasJore <jonas.jore@gmail.com>
2019-03-20 23:08:20 +0100

    forenklet sjekk på valideringsregler. Det blir også gitt visuelt beskjed om input er gyldig eller ikke til bruker :coffee:

--- assets/scripts/validation.js
+++ assets/scripts/validation.js
@@ -10,20 +10,41 @@ Object.freeze(validering)
 let validation = (email, subject, type, text) => {
   const emailRegEx = /^[a-å|0-9._%+-]+@[a-z|0-9.-]+\.[a-z]{2,}$/
   const validEmail = emailRegEx.test(String(email.toLowerCase()))
+  const hasErrorClass = 'form-group has-error'
+  const sucessClass = 'form-group has-success'
   const validationRules = []
   if(email === '' || !validEmail) {
     validationRules.push(validering.INVALID_MAIL)
+    document.getElementById('emailInputField').className = hasErrorClass
     console.log(validering.INVALID_MAIL)
-  } if(subject.length == 0) {
+  } else {
+    document.getElementById('emailInputField').className = sucessClass
+  }
+  
+  if(subject.length == 0) {
     validationRules.push(validering.INVALID_SUBJECT)
+    document.getElementById('subjectField').className = hasErrorClass
     console.log(validering.INVALID_SUBJECT)
-  } if(type === 'default') {
-    validationRules.push(validering.INVALID_TYPE_OF_SERVICE)
-    console.log(validering.INVALID_TYPE_OF_SERVICE)
-  } if(text.length === 0 || text.length >= 150) {
-    validationRules.push(validering.INVALID_TEXT)
-    console.log(validering.INVALID_TEXT)
+  } else {
+    document.getElementById('subjectField').className = sucessClass
   } 
+  
+  if(type === 'default') {
+    validationRules.push(validering.INVALID_TYPE_OF_SERVICE)
+    document.getElementById('typeOfServiceField').className = hasErrorClass
+    console.log(validering.INVALID_TYPE_OF_SERVICE)
+  } else {
+    document.getElementById('typeOfServiceField').className = sucessClass
+  }
+  
+  if(text.length === 0 || text.length >= 150) {
+    validationRules.push(validering.INVALID_TEXT)
+    const textField = document.getElementById('textField')
+    textField.className += hasErrorClass
+    console.log(validering.INVALID_TEXT)
+  } else {
+    document.getElementById('textField').className = sucessClass
+  }
   
   if(noErrors(validationRules)) {
     const preparedJson = prepareJsonObj(email, subject, type, text)
@@ -31,12 +52,13 @@ let validation = (email, subject, type, text) => {
   }
 }
 
-const validMail = validationArr => validationArr.includes(validering.INVALID_MAIL) ? false : true
-const validSubject = validationArr => validationArr.includes(validering.INVALID_SUBJECT) ? false : true
-const validTypeOfService = validationArr => validationArr.includes(validering.INVALID_TYPE_OF_SERVICE) ? false : true
-const validText = validationArr => validationArr.includes(validering.INVALID_TEXT) ? false : true
+// TODO: se om dette kan forenkles :I
+const validMail = validationArr => validationArr.includes(validering.INVALID_MAIL)
+const validSubject = validationArr => validationArr.includes(validering.INVALID_SUBJECT)
+const validTypeOfService = validationArr => validationArr.includes(validering.INVALID_TYPE_OF_SERVICE)
+const validText = validationArr => validationArr.includes(validering.INVALID_TEXT)
 
-let noErrors = validationArr => validMail(validationArr) && validSubject(validationArr) && validTypeOfService(validationArr) && validText(validationArr)
+let noErrors = validationArr => !validMail(validationArr) && !validSubject(validationArr) && !validTypeOfService(validationArr) && !validText(validationArr)
 
 let prepareJsonObj = (mail, subject, type, text) => {
   return JSON.stringify({
